0816140bec65b0b0f4256a7d9451e65d86383a0e
root <root@ip-172-31-43-225.ap-south-1.compute.internal>
2024-06-13 05:17:47 +0000

    second commit

--- a.java
+++ a.java
@@ -1,3 +1,10 @@
-import boto3
+import boto3()
 ec2.boto3= instances
-
+ec2.instances={ run_instances
+	image_id="ami-34rmahnas33"
+	instance_type="t2.micro"
+        count="3"
+        key_name="linuxkeypair"
+        security_group="sg-1222jdgdd88"
+        subnet="sub-233jdgsss"
+}	
