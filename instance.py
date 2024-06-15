ef31c4bc6269a8154a3b5b488ca706df5c122e82
rushi <lingammani2@gmail.com>
2024-06-15 07:26:25 +0000

    first commit

---
+++ instance.py
@@ -0,0 +1,10 @@
+import boto3
+resource=ec2.boto3('ec2')
+instances=ec2.create_instances(
+        ImageId="ami-33ami8888"
+        InstanceType="t2.micro"
+        MinCount="3"
+        MaxCount="6"
+        Security_Group_Ids="sg-sggs3535"
+        SubnetId="445dsgug"
+        )
